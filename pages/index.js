bb2faa31e03330f82e9ea6a00d56decec2c48f80
leyuri <leyuri97@gmail.com>
2022-11-22 23:28:37 +0900

    Preparing the Fronted Form

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useRef } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.vaule;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea type="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
